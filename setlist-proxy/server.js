e56ba56794ad67b1fdafabb2ec9412756893c835
0xgasc <gasolomonc@gmail.com>
2026-01-08 12:52:46 -0500

    Add Cache-Control header to CORS allowedHeaders

--- setlist-proxy/server.js
+++ setlist-proxy/server.js
@@ -142,7 +142,7 @@ app.use(cors({
   origin: true, // Allow all origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Cache-Control', 'Pragma'],
   exposedHeaders: ['Content-Length', 'Content-Type']
 }));
 
